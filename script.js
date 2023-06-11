6cddebe10214e68230c7b21db791fdf90ac0f66a
JB <93525600+I-IAMBATMAN-N@users.noreply.github.com>
2023-06-11 20:57:40 +0200

    [REFACTORING]

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 "use strict";
 
+// Prepare Functions
+// const fetchURLs = {
+//   all: "https://restcountries.com/v3.1/all",
+//   byName: "https://restcountries.com/v3.1/name/",
+// };
+
+// function fetchData(url = fetchURLs.all) {
+//   fetch(url)
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((responseData) => {
+//       const data = responseData;
+
+//       // console.clear();
+//       finalData = data;
+//       return data;
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// }
+
 const form = document.querySelector(".form");
 const input = document.querySelector(".input");
 const searchBtn = document.querySelector(".btn--input");
@@ -12,7 +35,23 @@ console.log(countrySection);
 
 let finalData;
 
-function getAll() {
+//
+const btnMode = document.querySelector(".btn--mode");
+const btnModeSpans = document.querySelectorAll(".btn--mode span");
+const body = document.querySelector("body");
+
+btnMode.addEventListener("click", function () {
+  btnModeSpans.forEach((span) => {
+    span.classList.toggle("active");
+    console.log("Done");
+  });
+  body.classList.toggle("mode");
+});
+
+/* DEFAULT --------------------------------------------------- */
+// onLoad
+// displays all countries on window load
+function displayAll() {
   fetch(`https://restcountries.com/v3.1/all`)
     .then((response) => {
       return response.json();
@@ -23,7 +62,7 @@ function getAll() {
       // console.clear();
       console.log(data);
 
-      displayRelevant(data);
+      displaySmall(data);
       finalData = data;
       return data;
     })
@@ -32,17 +71,39 @@ function getAll() {
     });
 }
 
-window.addEventListener("load", () => {
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-  });
-  getAll();
-});
+// displays relevant countries acc. to passed object
+function displaySmall(object) {
+  function articleListenner(articles) {
+    articles.forEach((article) => {
+      article.addEventListener("click", (e) => {
+        let text = e.target
+          .closest(".country")
+          .children[1].children[0].innerText.toLowerCase();
 
-function displayRelevant(object) {
+        //
+        fetch(`https://restcountries.com/v3.1/name/${text}`)
+          .then((response) => {
+            return response.json();
+          })
+          .then((responseData) => {
+            const [data] = responseData;
+
+            console.log(data);
+            displayLarge(data);
+
+            // return data;
+          })
+          .catch((err) => {
+            console.log("--------------------------------------", err);
+          });
+      });
+    });
+  }
+
+  //
   let element = ``;
   object.forEach((item) => {
-    // console.log(element);
+    //
     element += `
       <article class="country">
       <img src="${item.flags.png}" alt="${item.flags.alt}" class="img"/>
@@ -63,37 +124,52 @@ function displayRelevant(object) {
   section.innerHTML += element;
   articleListenner(section.querySelectorAll(".country"));
 }
+//
+// form functions
+// - call function displaying searched country
+form.addEventListener("submit", function () {
+  displaySearchedCountry(input.value);
+});
+//
+// onload calls function that displays all countries
+window.addEventListener("load", () => {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+  displayAll();
+});
 
-function getByRegion(region) {
-  if (region) {
-    fetch(`https://restcountries.com/v3.1/region/${region}`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((responseData) => {
-        const data = responseData;
-
-        console.clear();
-        console.log(data);
-
-        displayRelevant(data);
-        finalData = data;
-        // return data;
-      })
-      .catch((err) => {
-        console.log("--------------------------------------", err);
-      });
-  } else return;
-}
-
+//
+// regions option element onchange event listenner
 countries.addEventListener("change", function (e) {
+  //
+  function getByRegion(region) {
+    if (region) {
+      fetch(`https://restcountries.com/v3.1/region/${region}`)
+        .then((response) => {
+          return response.json();
+        })
+        .then((responseData) => {
+          const data = responseData;
+
+          console.clear();
+          console.log(data);
+
+          displaySmall(data);
+          finalData = data;
+          // return data;
+        })
+        .catch((err) => {
+          console.log("--------------------------------------", err);
+        });
+    } else return;
+  }
   getByRegion(
     e.target.value.search("Filter") > -1 ? undefined : e.target.value
   );
-  // console.log(e.target.value);
 });
 
-function findCountry(inputValue) {
+function displaySearchedCountry(inputValue) {
   let tempData = [];
   if (!inputValue.search("Filter") > -1) {
     finalData.forEach((country) => {
@@ -107,118 +183,31 @@ function findCountry(inputValue) {
       }
     });
     // console.log(finalData);
-    displayRelevant(tempData);
+    displaySmall(tempData);
   }
 }
-
-form.addEventListener("submit", function () {
-  console.log(input.value);
-  findCountry(input.value);
-});
-
-function articleListenner(articles) {
-  articles.forEach((article) => {
-    article.addEventListener("click", (e) => {
-      let text = e.target
-        .closest(".country")
-        .children[1].children[0].innerText.toLowerCase();
-
-      //
-      fetch(`https://restcountries.com/v3.1/name/${text}`)
+function displayLarge(country) {
+  //
+  function fillNeighbors(neighbours) {
+    function getCountryName(countryCode) {
+      fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`)
         .then((response) => {
           return response.json();
         })
         .then((responseData) => {
           const [data] = responseData;
 
-          console.log(data);
-          displayFullCountry(data);
+          console.log("country name", data.name.common);
 
-          // return data;
+          return data.name.common;
         })
         .catch((err) => {
           console.log("--------------------------------------", err);
         });
-    });
-  });
-}
-
-const btnMode = document.querySelector(".btn--mode");
-const btnModeSpans = document.querySelectorAll(".btn--mode span");
-const body = document.querySelector("body");
-
-btnMode.addEventListener("click", function () {
-  btnModeSpans.forEach((span) => {
-    span.classList.toggle("active");
-    console.log("Done");
-  });
-  body.classList.toggle("mode");
-});
-
-function displayFullCountry(country) {
-  //
-  let nativeName;
-  if (country.name.nativeName) {
-    console.log(country.name.nativeName);
-
-    console.log("laguages", country.languages);
-    console.log("neighbours", country.borders);
-
-    const { ...nativeNames } = country.name.nativeName;
-    const objKeys = Object.keys(country.name.nativeName);
-
-    nativeName = nativeNames[objKeys[0]].official;
-  } else {
-    console.log("NO NATIVE NAME");
-  }
-
-  let currString = "";
-  let langString = "";
-
-  const currencyKeys = Object.keys(country.currencies);
-  const languagesKeys = Object.keys(country.languages);
-
-  console.log(currencyKeys.length);
-  console.log(languagesKeys.length);
-
-  currencyKeys.forEach((key, index) => {
-    currString += key;
-    if (index < currencyKeys.length - 1) {
-      currString += ", ";
     }
-  });
-  languagesKeys.forEach((key, index) => {
-    langString += key;
-    if (index < languagesKeys.length - 1) {
-      langString += ", ";
-    }
-  });
-
-  console.log("currencies", country.currencies);
-  console.log("languages", languagesKeys);
-
-  console.log(country.capital[0]);
-
-  function fillNeighbors(neighbours) {
     if (neighbours) {
       let element = "";
 
-      function getCountryName(countryCode) {
-        fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`)
-          .then((response) => {
-            return response.json();
-          })
-          .then((responseData) => {
-            const [data] = responseData;
-
-            console.log(data);
-
-            return data;
-          })
-          .catch((err) => {
-            console.log("--------------------------------------", err);
-          });
-      }
       //
 
       neighbours.forEach((neighbour) => {
@@ -233,6 +222,43 @@ function displayFullCountry(country) {
         <li class="list-item">No Neighbour</li>
         `;
   }
+  //
+  function getNativeName() {
+    if (country.name.nativeName) {
+      //
+      const { ...nativeNames } = country.name.nativeName;
+      const objKeys = Object.keys(country.name.nativeName);
+
+      return nativeNames[objKeys[0]].official;
+    } else {
+      return "No Native Name";
+    }
+  }
+  //
+  function getCurrencies() {
+    let string = "";
+    const currencyKeys = Object.keys(country.currencies);
+    currencyKeys.forEach((key, index) => {
+      string += key;
+      if (index < currencyKeys.length - 1) {
+        string += ", ";
+      }
+    });
+    return string;
+  }
+  //
+  function getLanguages() {
+    let string = "";
+    const languagesKeys = Object.keys(country.languages);
+    languagesKeys.forEach((key, index) => {
+      string += key;
+      if (index < languagesKeys.length - 1) {
+        string += ", ";
+      }
+    });
+    return string;
+  }
+
   let element = `
     <article class="country-full">
       <img src="${country.flags.svg}" alt="${
@@ -240,7 +266,7 @@ function displayFullCountry(country) {
   } flag" width="500px" />
       <section>
         <h3 class="name">${country.name.official}</h3>
-        <h5 class="data">Native Name: <span>${nativeName}</span></h5>
+        <h5 class="data">Native Name: <span>${getNativeName()}</span></h5>
         <h5 class="data">
           Population: <span>${+(country.population / 1000000).toFixed(
             1
@@ -255,8 +281,8 @@ function displayFullCountry(country) {
         </h5>
         <br />
         <h5 class="data">Top Level Domain: <span>${country.tld}</span></h5>
-        <h5 class="data">Currencies: <span>${currencyKeys}</span></h5>
-        <h5 class="data">Languages: <span>${langString}</span></h5>
+        <h5 class="data">Currencies: <span>${getCurrencies()}</span></h5>
+        <h5 class="data">Languages: <span>${getLanguages()}</span></h5>
         <br />
         <h5 class="data">Neighbours: </h5>
         <ul class="list">
@@ -266,22 +292,16 @@ function displayFullCountry(country) {
     </article>
   `;
 
-  // console.log(element);
-
-  section.innerHTML = "";
-  section.style.display = "none";
-
   const inputContainers = form.querySelectorAll(".input-container");
-
-  console.log(inputContainers);
+  //
   inputContainers.forEach((container) => {
     container.classList.toggle("active");
   });
-
+  //
   form.innerHTML += `
-    <button> back </button>
+    <button class="btn--back"> back </button>
   `;
-  form.style.display = "none";
-
+  //
+  section.style.display = "none";
   countrySection.innerHTML += element;
 }
