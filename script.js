dbee1e6174bc8a49aa382091132ba8190d68d3e9
JB <93525600+I-IAMBATMAN-N@users.noreply.github.com>
2023-06-11 18:19:50 +0200

    [CREATE]displayFullCountry function
    - displays selected country with extended informations
    
    style.css
    - .full-country HTML element styling

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const countries = document.querySelector("#countries");
 
 const section = document.querySelector(".section");
 
+const countrySection = document.querySelector(".country-section");
+console.log(countrySection);
+
 let finalData;
 
 function getAll() {
@@ -17,7 +20,7 @@ function getAll() {
     .then((responseData) => {
       const data = responseData;
 
-      console.clear();
+      // console.clear();
       console.log(data);
 
       displayRelevant(data);
@@ -45,9 +48,13 @@ function displayRelevant(object) {
       <img src="${item.flags.png}" alt="${item.flags.alt}" class="img"/>
         <section> 
         <h3 class="country-name">${item.name.official}</h3>
-          <h5 class="data">Population: <span>${item.population} mil.</span></h5>
+          <h5 class="data">Population: <span>${(
+            +item.population / 1000000
+          ).toFixed(1)} mil.</span></h5>
           <h5 class="data">Region: <span>${item.region}</span></h5>
-          <h5 class="data">Capital: <span>${item.capital}</span></h5>
+          <h5 class="data">Capital: <span>${
+            item.capital || "no capital"
+          }</span></h5>
         </section>
       </article>
     `;
@@ -112,9 +119,10 @@ form.addEventListener("submit", function () {
 function articleListenner(articles) {
   articles.forEach((article) => {
     article.addEventListener("click", (e) => {
-      let text = e.target.closest(".country").children[1].innerText;
+      let text = e.target
+        .closest(".country")
+        .children[1].children[0].innerText.toLowerCase();
 
-      // return text;
       //
       fetch(`https://restcountries.com/v3.1/name/${text}`)
         .then((response) => {
@@ -124,6 +132,7 @@ function articleListenner(articles) {
           const [data] = responseData;
 
           console.log(data);
+          displayFullCountry(data);
 
           // return data;
         })
@@ -145,3 +154,134 @@ btnMode.addEventListener("click", function () {
   });
   body.classList.toggle("mode");
 });
+
+function displayFullCountry(country) {
+  //
+  let nativeName;
+  if (country.name.nativeName) {
+    console.log(country.name.nativeName);
+
+    console.log("laguages", country.languages);
+    console.log("neighbours", country.borders);
+
+    const { ...nativeNames } = country.name.nativeName;
+    const objKeys = Object.keys(country.name.nativeName);
+
+    nativeName = nativeNames[objKeys[0]].official;
+  } else {
+    console.log("NO NATIVE NAME");
+  }
+
+  let currString = "";
+  let langString = "";
+
+  const currencyKeys = Object.keys(country.currencies);
+  const languagesKeys = Object.keys(country.languages);
+
+  console.log(currencyKeys.length);
+  console.log(languagesKeys.length);
+
+  currencyKeys.forEach((key, index) => {
+    currString += key;
+    if (index < currencyKeys.length - 1) {
+      currString += ", ";
+    }
+  });
+  languagesKeys.forEach((key, index) => {
+    langString += key;
+    if (index < languagesKeys.length - 1) {
+      langString += ", ";
+    }
+  });
+
+  console.log("currencies", country.currencies);
+  console.log("languages", languagesKeys);
+
+  console.log(country.capital[0]);
+
+  function fillNeighbors(neighbours) {
+    if (neighbours) {
+      let element = "";
+
+      function getCountryName(countryCode) {
+        fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`)
+          .then((response) => {
+            return response.json();
+          })
+          .then((responseData) => {
+            const [data] = responseData;
+
+            console.log(data);
+
+            return data;
+          })
+          .catch((err) => {
+            console.log("--------------------------------------", err);
+          });
+      }
+      //
+
+      neighbours.forEach((neighbour) => {
+        console.log("neighbour-22", neighbour);
+        element += `
+        <li class="list-item">${neighbour}</li>
+        `;
+      });
+      return element;
+    } else
+      return `
+        <li class="list-item">No Neighbour</li>
+        `;
+  }
+  let element = `
+    <article class="country-full">
+      <img src="${country.flags.svg}" alt="${
+    country.flags.alt || country.name.official
+  } flag" width="500px" />
+      <section>
+        <h3 class="name">${country.name.official}</h3>
+        <h5 class="data">Native Name: <span>${nativeName}</span></h5>
+        <h5 class="data">
+          Population: <span>${+(country.population / 1000000).toFixed(
+            1
+          )} mil.</span>
+        </h5>
+        <h5 class="data">Region: <span>${country.region}</span></h5>
+        <h5 class="data">
+          Sub Region: <span>${country.subRegion || "no subregion"}</span>
+        </h5>
+        <h5 class="data">
+          Capital: <span>${country.capital[0] || "no capital"}</span>
+        </h5>
+        <br />
+        <h5 class="data">Top Level Domain: <span>${country.tld}</span></h5>
+        <h5 class="data">Currencies: <span>${currencyKeys}</span></h5>
+        <h5 class="data">Languages: <span>${langString}</span></h5>
+        <br />
+        <h5 class="data">Neighbours: </h5>
+        <ul class="list">
+            ${fillNeighbors(country.borders)}
+        </ul>
+      </section>    
+    </article>
+  `;
+
+  // console.log(element);
+
+  section.innerHTML = "";
+  section.style.display = "none";
+
+  const inputContainers = form.querySelectorAll(".input-container");
+
+  console.log(inputContainers);
+  inputContainers.forEach((container) => {
+    container.classList.toggle("active");
+  });
+
+  form.innerHTML += `
+    <button> back </button>
+  `;
+  form.style.display = "none";
+
+  countrySection.innerHTML += element;
+}
